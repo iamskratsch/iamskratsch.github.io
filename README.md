a92ee7c1b4c17dee77bc8cc6c7c91c60df01a78b
steve.kaschimer <steve.kaschimer@slalom.com>
2023-09-15 15:25:15 -0400

    tearing it all down again

--- README.md
+++
@@ -1,1 +0,0 @@
-# iamskratsch.github.io
--- src/utils/fontawesome.js
+++
@@ -1,4 +0,0 @@
-import { library } from "@fortawesome/fontawesome-svg-core"
-import { faxtwitter } from "@fortawesome/free-solid-svg-icons"
-import { falinkedin } from "@fortawesome/free-solid-svg-icons"
-library.add(faxtwitter, falinkedin)
